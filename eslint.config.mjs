e4084cbe3cdb03ce43bbf85239dbd17c66e9405b
Joy-Joy20 <gulpanjoy@gmail.com>
2026-04-26 16:19:03 +0800

    Add project files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,20 +5,14 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  // Override default ignores of eslint-config-next.
   globalIgnores([
+    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
-  {
-    rules: {
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-img-element": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "react-hooks/set-state-in-effect": "off",
-    },
-  },
 ]);
 
 export default eslintConfig;
